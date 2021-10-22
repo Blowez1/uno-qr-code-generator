0cc73e96c10a48568bbbd0a08a0abde90888c0af
Berke Batur <aberkebatur@gmail.com>
2021-10-22 22:07:50 +0300

    firstt

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<video autoplay muted loop id="background-video">
+  <source src="../assets/videos/background-video.mp4" type="video/mp4">
+</video>
+<div class="overlay-video"></div>
+</div>
+</template>
+
+<script>
+
+export default {
+  
+}
+ 
+</script>
